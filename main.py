c46da28c489bc5ddcc4e99b7283b9b686c5f5a34
Rama_Kumar_Reddy <ramkrg@outlook.com>
2021-10-17 10:32:43 +0530

    Created background images and text.

--- main.py
+++ main.py
@@ -5,7 +5,11 @@ pygame.init()
 screen = pygame.display.set_mode((800, 400))
 pygame.display.set_caption("Runner - A Game by Ram Kumar")
 clock = pygame.time.Clock()
+test_font = pygame.font.Font("font/Pixeltype.ttf", 50)
 
+sky_surface = pygame.image.load("graphics/Sky.png")
+ground_surface = pygame.image.load("graphics/ground.png")
+text_surface = test_font.render("My game", False, "Black")
 
 while True:
     for event in pygame.event.get():
@@ -14,5 +18,10 @@ while True:
             exit()
     # Draw all or elements.
     # Update everything.
+
+    screen.blit(sky_surface, (0, 0))
+    screen.blit(ground_surface, (0, 300))
+    screen.blit(text_surface, (300, 50))
+
     pygame.display.update()
     clock.tick(60)
